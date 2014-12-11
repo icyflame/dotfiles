57943e51eabc1f5958b6c98be3319fb0667ec44c
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-12-11 18:07:54 +0530

    Add a bash script to change the color scheme to solarized.
    This bash script has to be run from Home. More commands will be added to
    it as time goes on!

---
+++ runFromHome.sh
@@ -0,0 +1,9 @@
+# Install Zsh : https://github.com/robbyrussell/oh-my-zsh
+# Last Step: Copy zshrc from here instead of the zshrc template provided.
+
+# Setup the solarized color scheme.
+# https://gist.github.com/gmodarelli/5942850
+
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+./set_dark.sh
