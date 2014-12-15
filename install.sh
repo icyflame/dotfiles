4078ad68dba85b801cd4a098fa756807b5ec2da1
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-12-15 12:11:54 +0530

    Add the bash file that creates the symlinks.
    Copying of the old files to the dotfiles_old folder is not working.
    
    [TODO] Fix this.

---
+++ install.sh
@@ -0,0 +1,20 @@
+cd
+
+declare -r olddir="dotfiles_old"
+
+echo $olddir
+
+mkdir -p $olddir
+
+for file in `ls ~/dotfiles/*.symlink`; do
+	# echo $file
+	filename=$(basename "$file")
+	extension="${filename##*.}"
+	dotfile="${filename%.*}"
+	# echo $dotfile
+	# echo "--remove-destination ~/.$dotfile $olddir/$dotfile.old"
+	cp -u -v --remove-destination ~/.$dotfile $olddir/$dotfile.old
+	rm ~/.$dotfile
+	# echo "ln -s $file ~/.$dotfile"
+	ln -s $file ~/.$dotfile
+done
