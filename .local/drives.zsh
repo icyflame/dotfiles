c2b0295301a5793af0cea1c92f4e64ebcf60fd64
Siddharth Kannan <siddharthkannan@protonmail.com>
2016-09-10 11:42:16 +0530

    [local] Add some new aliases for local use
    Signed-off-by: Siddharth Kannan <siddharthkannan@protonmail.com>

--- .local/drives.zsh
+++ .local/drives.zsh
@@ -4,10 +4,13 @@ alias hiper="drived && cd hiperware/"
 alias anet="drived && cd alumni-cell-git-repo/networking-rails/"
 alias amentor="drived && cd alumni-cell-git-repo/mentorship-portal/mentorship-portal-private-repo"
 
+# acads
+alias btp='cd "/media/New Volume/btp-all"'
+alias acads='cd "/media/New Volume/KGP/KGP Acads/KGP Academics/Sem 7 -- Autumn 2016"'
+
 # switch drives and filesystems
 
 alias drived="cd /media/New\ Volume/"
 alias drivee="cd /media/General/"
-alias acads="cd /media/New\ Volume/KGP/KGP\ Acads/KGP\ Academics/Sem\ 6\ --\ Spring\ 2016/"
 
 alias this_included="test"
