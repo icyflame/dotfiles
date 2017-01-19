92a3f5162ee5339b3b98796c5b929df7d73d6066
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-01-19 19:49:33 +0530

    zsh: add function to convert markdown to pdf

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,49 +1,49 @@
 # function for long running commands
 # run as `long git push origin --all`
 function long {
-	eval $@;
-	# echo "This command was executed"
-	notify-send -t 2000 --urgency=low -i "terminal" "$@ done!"
+eval $@;
+# echo "This command was executed"
+notify-send -t 2000 --urgency=low -i "terminal" "$@ done!"
 }
 
 # function for searching the history for the string
 # that is passed as a parameter
 function hist {
-	history | ag $@
+history | ag $@
 }
 
 function calc {
-	echo $(($@));
+echo $(($@));
 }
 
 function compile {
-	gcc $@ && ./a.out
+gcc $@ && ./a.out
 }
 
 function gmp_compile {
-	g++ $@ -lgmpxx -lgmp && ./a.out
+g++ $@ -lgmpxx -lgmp && ./a.out
 }
 
 function verifyApk {
-	jarsigner -verify -verbose -certs $@ | less
+jarsigner -verify -verbose -certs $@ | less
 }
 
 function verifyApkSHAFingerPrint {
-	keytool -list -printcert -jarfile $@ | less
+keytool -list -printcert -jarfile $@ | less
 }
 
 function getAllSubtitles {
-	for i in $@; do
-		findsub "$i"
-	done;
+for i in $@; do
+	findsub "$i"
+done;
 }
 
 function cutVideo {
-	ffmpeg -i $1 -ss $2 -t $3 -c copy $4;
+ffmpeg -i $1 -ss $2 -t $3 -c copy $4;
 }
 
 function getAudio {
-	youtube-dl --extract-audio --prefer-ffmpeg --audio-format mp3 $@;
+youtube-dl --extract-audio --prefer-ffmpeg --audio-format mp3 $@;
 }
 
 # usage: ssh-setup user@IP
@@ -51,6 +51,21 @@ function getAudio {
 # tasks: and then copy over this computer's pub key to remote IP
 # https://github.com/TwP/dotfiles/blob/e8294e2330f3375516acf07e9cbf5fc0a72c4fb7/bash/aliases.bash#L142-L145
 function ssh-setup {
-	ssh $1 'mkdir -p -m 700 .ssh; touch .ssh/authorized_keys; chmod 600 .ssh/authorized_keys';
-	cat ~/.ssh/id_rsa.pub | ssh $1 'cat - >> ~/.ssh/authorized_keys'
+ssh $1 'mkdir -p -m 700 .ssh; touch .ssh/authorized_keys; chmod 600 .ssh/authorized_keys';
+cat ~/.ssh/id_rsa.pub | ssh $1 'cat - >> ~/.ssh/authorized_keys'
+}
+
+function mfarkdown_to_pdf {
+value=4
+if [ $value -eq 4 ]
+then
+	echo "YAY!"
+fi
+}
+
+function markdown_to_pdf {
+FILE_NAME=$(echo $1 | cut -d . -f 1);
+markdown $1 > "$FILE_NAME.html";
+wkhtmltopdf "$FILE_NAME.html" "$FILE_NAME.pdf"
+rm "$FILE_NAME.html"
 }
