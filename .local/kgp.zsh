71a5f77305b29cd8c04a7122d9a61117ff9052f3
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-03-30 13:18:52 +0530

    zsh: implement a proper check to see if in KGP before exporting proxy
    - controlled by exporting a variable in the ~/.localrc file

--- .local/kgp.zsh
+++ .local/kgp.zsh
@@ -1,6 +1,17 @@
 # proxy for KGP
-export http_proxy="http://10.3.100.207:8080"
-export https_proxy="http://10.3.100.207:8080"
-export HTTP_PROXY="http://10.3.100.207:8080"
-export HTTPS_PROXY="http://10.3.100.207:8080"
-export KGP_LOCAL_IMPORTED="true"
+if [ $KGP ]; then
+  PROXY_STRING="http://10.3.100.207:8080"
+  export no_proxy="$no_proxy,10.0.0.0/8"
+  echo "LOCAL: Importing KGP proxy settings"
+else
+  PROXY_STRING=""
+  echo "LOCAL: Direct connection to the Internet"
+fi
+export http_proxy="$PROXY_STRING"
+export https_proxy="$PROXY_STRING"
+export HTTP_PROXY="$PROXY_STRING"
+export HTTPS_PROXY="$PROXY_STRING"
+export ALL_PROXY="$PROXY_STRING"
+export all_proxy="$PROXY_STRING"
+export ftp_proxy="$PROXY_STRING"
+export socks_proxy="$PROXY_STRING"
