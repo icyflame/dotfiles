224d5e2ca56c1bbed42eb2fb1fdca5fdaf3c3f00
Siddharth Kannan <mail@siddharthkannan.in>
2019-02-20 20:04:45 +0900

    Add aliases for kubernetes related commands

---
+++ zsh/kubernetes.zsh
@@ -0,0 +1,24 @@
+alias kgpods="kubectl get pods"
+alias kgsvc="kubectl get services"
+
+alias kgpod="kubectl get pod -o json"
+alias kgsvc="kubectl get service -o json"
+
+alias kdpod="kubectl describe pod"
+alias kdsvc="kubectl describe service"
+
+alias kex="kubectl exec -it"
+
+alias klog="kubectl logs -f"
+
+alias kst="stern --exclude-container istio-proxy --since 1s"
+
+alias kg="kubectl get"
+
+alias st="stern --exclude-container istio-proxy --since 1s"
+alias sts="stern --exclude-container istio-proxy"
+
+alias kcon="kubectx"
+alias kns="kubens"
+
+alias kdef="kubens default"
