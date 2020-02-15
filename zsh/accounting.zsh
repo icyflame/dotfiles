079c1ab92e8f56010a5c5e87ef4b8855a92744ef
Siddharth Kannan <mail@siddharthkannan.in>
2020-02-15 18:31:20 +0900

    zsh: Add the docker oneliners for running ledger, hledger

---
+++ zsh/accounting.zsh
@@ -0,0 +1,7 @@
+function hledger () {
+    docker run --rm -v `pwd`:/data -w /data dastapov/hledger hledger $@
+}
+
+function ledger () {
+    docker run --rm -v `pwd`:/data -w /data dcycle/ledger:1 $@
+}
