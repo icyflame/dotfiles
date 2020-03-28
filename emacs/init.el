ddcaacceec320d65ad58ebd6766ca7df3aabf1b3
Siddharth Kannan <mail@siddharthkannan.in>
2020-03-28 15:37:51 +0900

    emacs: Add basic configuration

---
+++ emacs/init.el
@@ -0,0 +1,56 @@
+;; melpa
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode solarized-theme gnu-elpa-keyring-update ## evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; Evil mode settings
+(require 'evil)
+(evil-mode 1)
+
+;; Color scheme
+(load-theme 'solarized-dark t)
+
+;; Markdown mode
+(add-to-list 'auto-mode-alist '("\\.notes\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+;; Set the fill-column and wrap text at fill-column
+(setq-default fill-column 100)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;; Turn on indicator at fill column for all buffers
+(require 'fill-column-indicator)
+(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode 1)
+
+;; (setq fci-rule-width 1)
+;; (setq fci-rule-color "darkblue")
