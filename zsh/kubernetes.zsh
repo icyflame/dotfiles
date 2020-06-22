c0b39a5b92dfcaa98b5ac572c9cc68edc5aea764
Siddharth Kannan <mail@siddharthkannan.in>
2020-06-22 15:59:44 +0900

    zsh: kubernetes: Add a function to display logs in JSON

--- zsh/kubernetes.zsh
+++ zsh/kubernetes.zsh
@@ -2,6 +2,10 @@
 alias st="stern --exclude-container istio-proxy"
 alias sts="stern --exclude-container istio-proxy --since 1s"
 
+function stj {
+    sts -ojson "$1" | jq -c '.message | fromjson?'
+}
+
 function ste {
     sts -ojson "$1" | jq '.message | fromjson? | select(.level == "error")'
 }
