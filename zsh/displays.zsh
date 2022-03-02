b255669ab0075934b74965ae51a097448aed90d4
Siddharth Kannan <mail@siddharthkannan.in>
2022-03-02 21:34:52 +0900

    zsh: displays: Use xrandr to switch displays and select only one

---
+++ zsh/displays.zsh
@@ -0,0 +1,31 @@
+function disp-list {
+    xrandr | awk '/ connected / { print $1 }'
+}
+
+function disp-external-only {
+    xrandr --output eDP-1 --off
+    xrandr --output DP-1 --auto
+}
+
+function disp-external-and-internal {
+    xrandr --output eDP-1 --auto
+    xrandr --output DP-1 --auto
+}
+
+function disp-one-only {
+    local selected=$(disp-list | fzf)
+    disp-only $selected
+}
+
+function disp-only {
+    local selected=$1
+    echo "Turn on: $selected"
+    disp-list | while read disp; do
+        if [[ "$disp" != "$selected" ]];
+        then
+            echo "Turn off: $disp"
+            xrandr --output $disp --off;
+        fi
+    done
+    xrandr --output $selected --auto;
+}
