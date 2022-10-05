657585e1e4f02e93d34d8e36247c4953b3ba7cf0
Siddharth Kannan <mail@siddharthkannan.in>
2022-10-05 16:15:08 +0900

    zsh: docker: Add docker alias for using pandoc

--- zsh/docker-aliases.zsh
+++ zsh/docker-aliases.zsh
@@ -1,5 +1,10 @@
-# pandoc: convert between several file-formats
-# Example: pandoc -f input -t output input-file -o output-file
-# pandoc () {
-    # docker run -v "$PWD":/source jagregory/pandoc $@
-# }
+# pandock: convert between several file-formats using the dockerized version of
+# Pandoc.
+# Available images: https://github.com/pandoc/dockerfiles#available-images
+pandock () {
+	if [ ! -x `which docker` ];
+	then
+		echo "ERROR: Docker is required for pandock"
+	fi
+	docker run --rm --volume "$(pwd):/data" --user $(id -u):$(id -g) pandoc/latex:2.19.2-alpine $@
+}
