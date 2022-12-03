bbec1c6088226ed87c0bffe2a43038576bd2ed65
Siddharth Kannan <mail@siddharthkannan.in>
2022-12-03 12:01:52 +0900

    zsh: docker: Add check_docker to see if the daemon is running

--- zsh/docker-aliases.zsh
+++ zsh/docker-aliases.zsh
@@ -2,11 +2,23 @@
 # Pandoc.
 # Available images: https://github.com/pandoc/dockerfiles#available-images
 
-pandock () {
+check_docker () {
 	if [ ! -x `which docker` ];
 	then
 		echo "ERROR: Docker is required for pandock"
+		return 42
 	fi
+
+	docker version > /dev/null
+	if [ $? -ne 0 ];
+	then
+		echo "ERROR: Docker executable exists. Docker daemon is not running."
+		return 43
+	fi
+}
+
+pandock () {
+	check_docker
 	docker run --rm --volume "$(pwd):/data" --user $(id -u):$(id -g) pandoc/latex:2.19.2-alpine $@
 }
 
@@ -15,5 +27,11 @@ dock_pandoc () {
 }
 
 dock_excalidraw () {
+	check_docker
 	docker run --rm -dit --name excalidraw -p 5000:80 excalidraw/excalidraw:latest
+}
+
+dock_pdflatex () {
+	check_docker
+	docker run --rm --volume "$(pwd):/data" --user $(id -u):$(id -g) --entrypoint pdflatex pandoc/latex:2.19.2-alpine $@
 }
