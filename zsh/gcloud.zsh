cc05c86436e38536a39cce166ae2fdb7c59d7747
Siddharth Kannan <mail@siddharthkannan.in>
2023-10-24 17:10:52 +0900

    zsh: gcloud: Add oneliner to translate SHA256 digest into an image tag

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -11,3 +11,30 @@ function gcauth {
 function gcexpiry {
     curl -s -H "Content-Type: application/x-www-form-urlencoded" -d "access_token=$(gcloud auth print-access-token)" https://oauth2.googleapis.com/tokeninfo | jq -r .expires_in
 }
+
+function gcr-image-tag {
+	local image=$1
+	local digest=$2
+
+	if [[ -z "$image" || -z "$digest" ]];
+	then
+		cat <<EOF
+ERROR: image-name and digest are required arguments
+
+COMMAND:
+
+	gcr-image-tag image-name digest
+
+EXAMPLE:
+
+	gcr-image-tag gcr.io/cloud-spanner-emulator/emulator cd2bc121ab99
+	DIGEST        TAGS   TIMESTAMP
+	cd2bc121ab99  1.4.5  2022-09-07T22:35:12
+EOF
+		return 43
+	fi
+
+	cmd="gcloud container images list-tags $image --filter='digest:$2'"
+	echo $cmd
+	eval $cmd
+}
