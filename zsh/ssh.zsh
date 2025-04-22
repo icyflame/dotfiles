73d390f672c9410267108c0ab2d8983497583158
Siddharth Kannan <skannan@gitlab.com>
2025-04-22 13:00:00 +0900

    ssh: Add the ability to specify SSH user
    SSH user might need to be specified because there are multiple users associated with the provided
    SSH key and one cannot be chosen or the wrong one is chosen.

--- zsh/ssh.zsh
+++ zsh/ssh.zsh
@@ -1,8 +1,18 @@
 function ssh_knife() {
+	local ssh_user=$1
 	if command -v knife >/dev/null 2>&1 && \
 			command -v fgrep >/dev/null 2>&1 && \
 			command -v fzf >/dev/null 2>&1;
-	then ssh $(knife node list | fgrep -v INFO | fzf);
+	then
+		selected_host=$(knife node list | fgrep -v INFO | fzf);
+		echo "SSH to $selected_host" >/dev/stderr;
+		if [ -n "$ssh_user" ];
+		then
+			echo "SSH as $ssh_user" >/dev/stderr;
+			ssh $ssh_user@$selected_host;
+		else
+			ssh $selected_host;
+		fi
 	else echo "ERROR: knife, fgrep, and fzf must be present."
 	fi
 }
