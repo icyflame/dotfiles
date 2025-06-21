1abb9c78d4c1bde4fd3a50d4b3ccfbe13aac9038
Siddharth Kannan <mail@siddharthkannan.in>
2025-06-21 11:10:26 +0900

    zsh: Use dock_ prefix for all Docker shorthand commands

--- zsh/accounting.zsh
+++ zsh/accounting.zsh
@@ -1,7 +1,7 @@
-function hledger () {
-    docker run --rm -v "$(pwd):/data" -w /data dastapov/hledger hledger $@
+function dock_hledger () {
+    docker run --rm -v "$(pwd):/data" -w /data dastapov/hledger:1.42.1 hledger $@
 }
 
-function ledger () {
+function dock_ledger () {
     docker run --rm -v "$(pwd):/data" -w /data dcycle/ledger:1 $@
 }
